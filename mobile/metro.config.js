7a0e7aa2cfe8e29dc26d294781e4437618c574d5
Alejandro <alejandromejiasramirez2006@gmail.com>
2026-03-22 16:15:12 +0000

    chore(mobile): update project configuration and resolve dependency warnings

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,5 +1,23 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+const workspaceRoot = path.resolve(__dirname, '..');
+const sharedRoot = path.resolve(workspaceRoot, 'shared');
+
+config.watchFolders = [sharedRoot];
+
+config.resolver.extraNodeModules = {
+    firebase: path.resolve(__dirname, 'node_modules/firebase'),
+    'firebase/firestore': path.resolve(__dirname, 'node_modules/firebase/firestore'),
+    'firebase/auth': path.resolve(__dirname, 'node_modules/firebase/auth'),
+    'firebase/app': path.resolve(__dirname, 'node_modules/firebase/app'),
+    '@': path.resolve(__dirname),
+};
+
+config.resolver.nodeModulesPaths = [
+    path.resolve(__dirname, 'node_modules'),
+];
 
 module.exports = config;
